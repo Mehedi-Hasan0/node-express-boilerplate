7a9e1c57d05ee95b904cb49651cff8ef376c04e4
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2025-09-19 12:08:52 +0600

    ADD: Cors options and body parser

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 // src/index.ts
 import express, { Request, Response, Application } from 'express';
+import config from './config';
+import cors, { CorsOptions } from 'cors';
 
 const app: Application = express();
-const port = 8000;
+const port = config.port;
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    callback(null, origin || '*');
+  },
+  credentials: true,
+};
+
+// cors config
+app.use(cors(corsOptions));
+
+// parse body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
